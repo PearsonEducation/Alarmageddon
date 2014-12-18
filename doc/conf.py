f88a8b6c66186f521f12243eb2fadcea5bc03861
Scott Hellman <scott@scutellaris.com>
2014-12-18 14:23:44 -0700

    fix copyright on docs

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Alarmageddon'
-copyright = u'2014, Tim Stewart and Scott Hellman'
+copyright = u'2014, Pearson'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
